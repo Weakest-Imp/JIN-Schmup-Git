68fa648fd58f1b19422ea487b502f1bf2151b00e
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2019-09-19 15:32:48 +0200

    No title on the title screen
    Created a title screen
    Added backgrounds on the two scenes

---
+++ JIN Schmup/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Animator explo;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("PoulpScene");
+    }
+
+    public void NotQuit() {
+        explo.SetTrigger("Explosion");
+    }
+}
