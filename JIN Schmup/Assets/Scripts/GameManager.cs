e2e782870e450c2a47b8678e08b1d1a5b5beaa16
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2019-09-19 15:00:39 +0200

    Managing the game
    Created a game Manager that instantiate everything

---
+++ JIN Schmup/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject poulp;
+    [SerializeField] private float poulpPopTime = 1.5f;
+    [SerializeField] private float poulpX;
+    [SerializeField] private float poulpMinY;
+    [SerializeField] private float poulpMaxY;
+
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        Instantiate(player, transform.position, transform.rotation);
+        StartCoroutine(PoulpPop());
+    }
+
+    IEnumerator PoulpPop() {
+        float poulpY = 0;
+        while (true) {
+            poulpY = Random.Range(poulpMinY, poulpMaxY);
+            Instantiate(poulp, new Vector3(poulpX, poulpY, 0), Quaternion.Euler(0, 0, 0));
+            yield return new WaitForSeconds(poulpPopTime);
+        }
+    }
+}
